467b4a2c3333155176e7ced5b004337ec194afd6
Keith Kelleher <keith.kelleher@nih.gov>
2020-09-08 13:09:10 -0400

    lower viral cutoff

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@
 const CONSTANTS = {
     DESCRIPTION_TYPE: 'NCBI Gene Summary',
     DEFAULT_PPI_CONFIDENCE: 400,
-    VIRAL_LR_CUTOFFF: 1000
+                VIRAL_LR_CUTOFFF: 500
 };
 module.exports = CONSTANTS;
