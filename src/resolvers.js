9ca0041519f4aae72d44e194ff269ecddecd9505
Keith Kelleher <keith.kelleher@nih.gov>
2021-06-16 13:34:15 -0400

    move reactome links to idg version of the tool

--- src/resolvers.js
+++ src/resolvers.js
@@ -715,7 +715,7 @@ const resolvers = {
                 });
                 outRows.forEach(row => {
                     if (row.type == "Reactome") {
-                        row.url = `https://reactome.org/PathwayBrowser/#/${row.sourceID}&FLG=${target.sym}`;
+                        row.url = `https://idg.reactome.org/PathwayBrowser/#/${row.sourceID}&FLG=${target.sym}`;
                     }
                 });
                 return outRows;
