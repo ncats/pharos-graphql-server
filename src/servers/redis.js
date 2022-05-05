6cd59f86cd290e77c7f015dea668e0ed0d43d1a2
KeithKelleher <keith.kelleher@ncats.nih.gov>
2022-05-05 17:05:11 -0400

    use gcp redis cache

--- src/servers/redis.js
+++ src/servers/redis.js
@@ -3,6 +3,7 @@ const {cred} = require("../db_credentials");
 
 module.exports.connectToRedis = () => {
     const REDISHOST = process.env.REDISHOST || 'localhost';// || '10.120.0.3';
+    console.log(REDISHOST);
     const REDISPORT = process.env.REDISPORT || 6379;
     let redisClient = new Redis({
         host: REDISHOST,
@@ -10,6 +11,10 @@ module.exports.connectToRedis = () => {
         keyPrefix: cred.CONFIGDB,
         lazyConnect: true
     });
+    redisClient.on('error',(err) => {
+        console.log(err);
+        redisClient.disconnect();
+    })
     try {
         return redisClient.connect().then((res) => {
             return redisClient;
