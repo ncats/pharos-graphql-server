59be539930ed192339cdb21ae313dab61daa1547
KeithKelleher <keith.kelleher@ncats.nih.gov>
2022-07-07 09:16:43 -0400

    use new TCRD

--- src/db_credentials.js
+++ src/db_credentials.js
@@ -3,7 +3,7 @@
 //
 const cred = {
     DBHOST: 'tcrd-cluster.cluster-ceyknq0yekb3.us-east-1.rds.amazonaws.com',
-    DBNAME: 'tcrd704',
+    DBNAME: 'tcrd705',
     USER: 'tcrd',
     PWORD: '',
     CONFIGDB: 'pharos_config_prod',
