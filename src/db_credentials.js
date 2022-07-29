3cfff2bb4984419a0de6e4efce2616b290a5c73a
KeithKelleher <keith.kelleher@ncats.nih.gov>
2022-07-29 10:05:33 -0400

    hierarchy trees for expression, diseases, & tinx

--- src/db_credentials.js
+++ src/db_credentials.js
@@ -3,7 +3,7 @@
 //
 const cred = {
     DBHOST: 'tcrd-cluster.cluster-ceyknq0yekb3.us-east-1.rds.amazonaws.com',
-    DBNAME: 'tcrd6124expr',
+    DBNAME: 'tcrd6124expr2',
     USER: 'tcrd',
     PWORD: '',
     CONFIGDB: 'pharos_config_prod',
